20ff60a5a71b6d6555ae9640a5b7240485d9d5eb
Tim Mortimer <tim@kiteframe.co.uk>
2024-09-30 10:12:37 +0100

    Extract get greeting route handler function

--- src/main/kotlin/uk/co/kiteframe/helloworld/HelloWorldApi.kt
+++ src/main/kotlin/uk/co/kiteframe/helloworld/HelloWorldApi.kt
@@ -1,9 +1,6 @@
 package uk.co.kiteframe.helloworld
 
-import org.http4k.core.HttpHandler
-import org.http4k.core.Method
-import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.*
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Undertow
@@ -16,6 +13,8 @@ fun main() {
 fun api(): HttpHandler {
     val helloWorld = HelloWorld()
     return routes(
-        "/greet" bind Method.GET to { Response(Status.OK).body(helloWorld.greet()) }
+        "/greet" bind Method.GET to getGreeting(helloWorld)
     )
 }
+
+private fun getGreeting(helloWorld: HelloWorld) = { _: Request -> Response(Status.OK).body(helloWorld.greet()) }
