4953a32858b34d4752cc1d0045f405983ef52df9
Tim Mortimer <tim@kiteframe.co.uk>
2024-09-30 11:31:37 +0100

    Prepare testing framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,10 @@ dependencies {
     implementation(platform("org.http4k:http4k-bom:5.32.1.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-server-undertow")
+    implementation("io.arrow-kt:arrow-core:1.2.4")
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.0")
 }
 
 tasks.test {
--- src/test/kotlin/uk/co/kiteframe/helloworld/HelloWorldTest.kt
+++ src/test/kotlin/uk/co/kiteframe/helloworld/HelloWorldTest.kt
@@ -1,5 +1,6 @@
 package uk.co.kiteframe.helloworld
 
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -7,6 +8,10 @@ class HelloWorldTest {
     @Test
     fun `it greets the world`() {
         val helloWorld = HelloWorld()
+
         assertEquals("Hello, world", helloWorld.greet())
+
+        // or, with Kotest assertions
+        helloWorld.greet() shouldBe "Hello, world"
     }
 }
